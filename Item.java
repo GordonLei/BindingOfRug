cb5a04d9ceca6bdd2be3798ee071482edbe889e0
ykadir42 <ykadir42@users.noreply.github.com>
2017-06-08 16:15:07 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,8 +1,28 @@
 public class Item{
-    public static String[] TYPES = {"sword", "armor", "spell"};
-    private String _type;
+    public static String[] TYPES = {"sword", "armor"}
+    public static String[] RARITY = {"common", "rare", "epic", "legendary"}
+    private String _type, _rarity;
     private Effect[] _effects;
+    
+    public Item(){
+        _type = TYPES[(int)(Math.random()*2)];
+        rarityIndex=Math.random();
+        if(rarityIndex<0.60){ _rarity="common"; _effects = new Effect[2];}
+        else if(rarityIndex<0.85){ _rarity="rare"; _effects = new Effect[3];}
+        else if(rarityIndex<0.975){ _rarity="epic"; _effects = new Effect[4];}
+        else {_rarity="legendary"; _effects = new Effect[5];}
+        generateEffects();
+    }
+    
+    public void generateEffects(){
+        switch(rarity){
+            case "common":
+                do{
+                    
+                }while(_effects.);
+            
+        }
 
-
+    }
 
 }
