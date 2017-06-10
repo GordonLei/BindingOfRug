73cd02201cda861987ff82647b73211e912c71e7
ykadir42 <ykadir42@gmail.com>
2017-06-10 02:32:57 -0400

    Finished Item and Effects Basics.

--- Effect.java
+++ Effect.java
@@ -1,13 +1,71 @@
 public class Effect{
-	private String _type;
+	public static final String[][] EFFECT_LIST=
+	{
+		//general effects
+		{"VIS", "2"},
+		{"SPD", "2"},
+		{"DEX", "1"},
+		{"HP", "1"},
+		{"LUK", "1"},
+		//{},
+
+		//sword effects
+		{"ATK", "1"},
+		{"ACC", "1"},
+		{"DOT", "1"},
+		//{},
+		//{},
+		//{},
+
+		//armor effects
+		{"DEF", "1"},
+		{"THRN", "1"},
+		//{},
+		//{},
+		//{},
+		//{},
+
+	};
+	private String _itemType;
+	private String[] _effect;
 	private int _power;
-	
-	public Effect(String type){
-    	_type=type;
+
+	public Effect(String itemType){
+    	_itemType=itemType;
 		init();
 	}
-	
+
+	public int getPower(){
+		return _power;
+	}
+
+	public String[] getEffect(){
+		return _effect;
+	}
+
 	public void init(){
-			
+		double index = Math.random();
+		switch(_itemType){
+			case "Sword":
+				if(index<0.5) _effect=EFFECT_LIST[(int)(Math.random()*5)];
+				else _effect=EFFECT_LIST[5+(int)(Math.random()*3)];
+				break;
+			case "Armor":
+				if(index<0.5) _effect=EFFECT_LIST[(int)(Math.random()*5)];
+				else _effect=EFFECT_LIST[8+(int)(Math.random()*2)];
+				break;
+		}
+		_power=(int)(Math.random()*5)-2;
+	}
+
+	public String toString(){
+		if (_power<0) return _effect[0]+_power;
+		return _effect[0]+"+"+_power;
+	}
+
+	public static void main(String[] args){
+		System.out.println(new Effect("Sword"));
+		System.out.println(new Effect("Armor"));
+
 	}
 }
--- Item.java
+++ Item.java
@@ -1,59 +1,68 @@
-import static java.Math.random;
-
 public class Item{
-    public static String[] TYPES = {"sword", "armor"}
-    public static String[] RARITY = {"common", "rare", "epic", "legendary"}
+    public static String[] TYPES = {"Sword", "Armor"};
+    public static String[] RARITY = {"Common", "Rare", "Epic", "Legendary"};
     private String _type, _rarity;
     private Effect[] _effects;
-    
+
     public Item(){
-        _type = TYPES[(int)(random()*2)];
-        rarityIndex=random();
-        if(rarityIndex<0.60){ _rarity="common"; _effects = new Effect[2];}
-        else if(rarityIndex<0.85){ _rarity="rare"; _effects = new Effect[3];}
-        else if(rarityIndex<0.975){ _rarity="epic"; _effects = new Effect[4];}
-        else {_rarity="legendary"; _effects = new Effect[5];}
+        _type = TYPES[(int)(Math.random()*2)];
+        double rarityIndex=Math.random();
+        if(rarityIndex<0.60){ _rarity="Common"; _effects = new Effect[2];}
+        else if(rarityIndex<0.85){ _rarity="Rare"; _effects = new Effect[3];}
+        else if(rarityIndex<0.975){ _rarity="Epic"; _effects = new Effect[4];}
+        else {_rarity="Legendary"; _effects = new Effect[5];}
         generateEffects();
     }
-    
+
     public void generateEffects(){
         int effectPower=0;
-        switch(rarity){
-            case "common":
+        switch(_rarity){
+            case "Common":
                 do{
-                    for(int i = 0; i<_effects.size; i++){
+                    effectPower=0;
+                    for(int i = 0; i<_effects.length; i++){
                         _effects[i]=new Effect(_type);
-                        effectPower+=_effects[i].getPower;
+                        effectPower+=_effects[i].getPower()*Integer.parseInt(_effects[i].getEffect()[1]);
                     }
-                }while(effectPower>1+BindingOfRug.getFloor()&&effectPower<3+BindingOfRug.getFloor());
+                }while(!(effectPower>1)||!(effectPower<3));
                 break;
-            case "rare":
+            case "Rare":
                 do{
-                    for(int i = 0; i<_effects.size; i++){
+                    for(int i = 0; i<_effects.length; i++){
                         _effects[i]=new Effect(_type);
-                        effectPower+=_effects[i].getPower;
+                        effectPower+=_effects[i].getPower()*Integer.parseInt(_effects[i].getEffect()[1]);
                     }
-                }while(effectPower>2+BindingOfRug.getFloor()&&effectPower<4+BindingOfRug.getFloor());
+                }while(!(effectPower>2)||!(effectPower<4));
                 break;
-            case "epic":
+            case "Epic":
                 do{
-                    for(int i = 0; i<_effects.size; i++){
+                    for(int i = 0; i<_effects.length; i++){
                         _effects[i]=new Effect(_type);
-                        effectPower+=_effects[i].getPower;
+                        effectPower+=_effects[i].getPower()*Integer.parseInt(_effects[i].getEffect()[1]);
                     }
-                }while(effectPower>3+BindingOfRug.getFloor()&&effectPower<5+BindingOfRug.getFloor());
+                }while(!(effectPower>3)||!(effectPower<5));
                 break;
-            case "legendary":
+            case "Legendary":
                 do{
-                    for(int i = 0; i<_effects.size; i++){
+                    for(int i = 0; i<_effects.length; i++){
                         _effects[i]=new Effect(_type);
-                        effectPower+=_effects[i].getPower;
+                        effectPower+=_effects[i].getPower()*Integer.parseInt(_effects[i].getEffect()[1]);
                     }
-                }while(effectPower>4+BindingOfRug.getFloor()&&effectPower<6+BindingOfRug.getFloor());
+                }while(!(effectPower>4)||!(effectPower<6));
                 break;
-            
+
         }
 
+    }
+
+    public String toString(){
+        String ans = _rarity +" "+ _type;
+        for(Effect effect:_effects) ans+="\n\t"+effect;
+        return ans;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Item());
     }
 
 }
