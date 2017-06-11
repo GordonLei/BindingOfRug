9341721c1aa4d4d5e82a70e60690132e93cd4f58
NekoYuuki <gordonlei2000@gmail.com>
2017-06-11 14:34:47 -0400

    Use in-class version

--- lib/ArrayQueue.java
+++ lib/ArrayQueue.java
@@ -1,78 +1,93 @@
 package lib;
 
 public class ArrayQueue<E> implements Queue<E>{
-        private E[] _queue;
-        private int _size, _front, _rear;
-        private final static int CAPACITY = 10;
 
-        //_rear holds the next empty slot
-        public ArrayQueue(int capacity){
-                _queue = (E[]) (new Object[capacity]);
-                _size = _rear = 0;
-                _front = -1;
-        }
+    private E[] _queue;
+    private int _size, _front, _rear;
+    private final static  int CAPACITY = 1000;
 
-        public ArrayQueue(){
-                this(CAPACITY);
-        }
 
-        public int size(){
-                return _size;
-        }
-        public E front(){
-                if (empty())
-                        throw new EmptyQueueException("Empty Queue");
-                return _queue[_front];
-        }
-        public boolean empty(){
-                return !(_size == _queue.length);
-        }
-        public E dequeue() throws EmptyQueueException{
-                if (empty())
-                        throw new EmptyQueueException("Empty Queue");
-                E temp = _queue[_front];
-                _queue[_front] = null;
-                _front = (_front + 1) % _queue.length;
-                _size--;
-                if (empty()){
-                        _rear = 0;
-                        _front = -1;
-                }
-                return temp;
-        }
-        public void enqueue(E value) throws FullQueueException{
-                if (isFull())
-                        throw new FullQueueException("Full Queue");
-                if (empty())
-                        _front = 0;
-                _queue[_rear] = value;
-                _size++;
-                _rear = (_rear + 1) % _queue.length;
-        }
-        public boolean isFull(){
-                return (_front == _rear);
-        }
+    // _rear == _front only when the queue is full
+    // otherwise _rear refers to an empty location
+    // When the queue is empty, The Empty State, _front = -1, _rear = 0.
 
-        public String toString(){
-                String ans = "[";
-                for (int i = _front; i < _front + _size; i++){
-                        ans += _queue[i % _queue.length] + ",";
-                }
-                if (!empty())
-                        ans = ans.substring(0, ans.length() - 2);
-                ans += "]";
-                /*
-                if (size() == 0){
-                        ans+= front();
-                }
-                else{
-                        for (int i = _front; i < _front + _size; i++){
-                                ans += _queue[i % _queue.length] + ", ";
-                        }
-                        ans += "]";
-                }
-                */
-                return ans;
-        }
+    public ArrayQueue(int capacity){
+	_queue = (E[]) new Object[capacity]; // produces a compiler warning - ignore it
+	_size = _rear = 0;
+	_front = -1; // optional
+    }
+
+    public ArrayQueue(){
+	this(CAPACITY);
+    }
+
+    public int size(){
+	return _size;
+    }
+
+    public boolean empty(){
+	return size() == 0;
+    }
+
+    public E front() throws EmptyQueueException{
+	if (empty())
+	    throw new EmptyQueueException("Queue is empty.");
+	return _queue[_front];
+    }
+
+    public E dequeue() throws EmptyQueueException{
+	E ans = front();
+	_queue[_front] = null; // release the object
+	_size--;
+	if (!empty())
+	    _front = (1 + _front) % _queue.length; //  wrap _front around the array when necessary
+	else{// return to the empty state
+	    _front = -1;
+	    _rear = 0;
+	}
+	return ans;
+    }
+
+    public void enqueue(E value) throws FullQueueException{
+	if (size() == _queue.length)
+	    throw new FullQueueException("Full stack.");
+	_queue[_rear] = value;
+	_size++;
+	if (size() == 1) _front = _rear; // transition out of the empty state
+	_rear = (_rear + 1) % _queue.length;
+    }
+
+    // postcondition :returns a String representing the ArrayQueue.
+    //                "[]" is an empty queue
+    //                [5,10,15] where 5 is at the front of the queue.
+    public String toString(){
+	String ans ="[";
+	// Your Code Goes Here
+        ans += "]";
+        return ans;
+    }
+
+    public static void main(String [] args){
+	Queue<Integer> q = new ArrayQueue<Integer>(10);
+	System.out.println(q);
+
+	for (int i = 0; i < 5 ; i++){
+	    q.enqueue(i);
+	    System.out.println("enqueue : " + i + " q: " + q);
+	}
+
+	while (!q.empty()){
+	    if (Math.random() < 0.75)
+		System.out.println("dequeue " + q.dequeue() + " q: " + q);
+	    else {
+		int x = (int)(Math.random() * 100) ;
+		q.enqueue(x);
+		System.out.println("enqueue : " + x + " q: " + q);
+	    }
+	}
+
+    }
+
+
 
 }
