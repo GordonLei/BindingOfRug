77d1c62df20eba409a1941476116102186db2e32
NekoYuuki <gordonlei2000@gmail.com>
2017-06-11 21:23:36 -0400

    added inventory

--- Player.java
+++ Player.java
@@ -1,14 +1,14 @@
 public class Player extends Creature{
 	//instance variables
 	private static int _health, _speed, _attack;
-
+	private static Item _inventory;
 
 	//Player starting stats
 	public Player(){
 		_health = 100;
 		_speed = 1;
 		_attack = 25;
-
+		_inventory = null;
 	}
 
 	//get representation of the Player
@@ -32,6 +32,10 @@ public class Player extends Creature{
 	//checks if the creature is dead
 	public boolean isDead(){
 		return (_health <= 0);
+	}
+
+	public void setInventory(Item newItem){
+		_inventory = newItem;
 	}
 
 }
